a600d081985fd623278878f1ca50cc2841b78a99
Peter Helenefors <peter@hertigcarl.se>
2022-01-30 23:56:40 +0100

    :shrug:

--- hero.js
+++ hero.js
@@ -1,3 +1,14 @@
+function equals(arr1, arr2) {
+    for(let i=0;i<3;i++) 
+        if(arr1[i] !== arr2[i])
+            return false;
+    
+    return true
+}
+
+const ladderTmpl = Uint8Array.from([255,0,0]);
+const freeFallTmpl = Uint8Array.from([0,0,0]);
+
 class Hero {
     constructor() {
         this.drawing = new Image();
@@ -18,31 +29,53 @@ class Hero {
     render(ctx, env) {
         let movementChange = env.movementSpec;
 
-        var pixelAtFeet = ctx.getImageData(env.elsaPos[0] + (this.drawing.width/2), env.elsaPos[1] + this.drawing.height, 1, 1).data;
-        var pixelBelowFeet = ctx.getImageData(env.elsaPos[0] + (this.drawing.width/2), env.elsaPos[1] + this.drawing.height + 1, 1, 1).data;
-        
-        if(pixelAtFeet[0] === 255) {
-            console.log('at ladder')
-            env.elsaPos[1] += movementChange[1]// + movementJump[1];
-        }
+        let pxHeroCenter =env.elsaPos[0] + (this.drawing.width/2);
+        let pxHeroBottom = env.elsaPos[1] + this.drawing.height
 
+        let pixelAtFeet = ctx.getImageData(pxHeroCenter, pxHeroBottom, 1, 1).data;
+        // let pixelBelowFeet = ctx.getImageData(pxHeroCenter, pxHeroBottom + 1, 1, 1).data;
 
-        // let movementJump = env.movements.shift() || [0,0];
-        if ( pixelBelowFeet.slice(0,3).some(x => x === 255)) {
-            // allow climb
-            // if(movementJump[1] > 0 ) {
-                // env.movements =[]
-                // console.log(pixelBelowFeet.join(','), ' solid')
-                // movementJump = [0,0]
-            // }
-            // env.elsaPos[1] += movementChange[1]// + movementJump[1];
+        let i = 0;
+        let pxCurrent = ctx.getImageData(pxHeroCenter, pxHeroBottom+i, 1, 1).data;
+
+        console.log('movement',movementChange[0])
+        if(movementChange[1] !== 0){
+            let pm = movementChange[1] / Math.abs(movementChange[1])
+            while(equals(pxCurrent, ladderTmpl))
+            {
+                i+=1*pm
+                console.log('At ladder. Eg accept up/downwards movement')
+                pxCurrent = ctx.getImageData(pxHeroCenter, pxHeroBottom+Math.abs(i)+1, 1, 1).data;
+                // debugger
+                if(Math.abs(i) > Math.abs(env.stepLength))
+                    break;
+                console.log(i, env.elsaPos[1], pxCurrent)
             }
- else {
-    env.elsaPos[1] += 1
-    // movementJump[1]= 10
+            
         }
-
         
+        env.elsaPos[1] += i// + movementJump[1];
+
+        if(i === 0) {
+            pxCurrent = ctx.getImageData(pxHeroCenter, pxHeroBottom+i, 1, 1).data;
+            
+            while(equals(pxCurrent, freeFallTmpl)) {
+                i++;
+                console.log(i, 'not solid')
+                pxCurrent = ctx.getImageData(pxHeroCenter, pxHeroBottom+i, 1, 1).data;
+                if(i > env.stepLength)
+                    break;
+                console.log(i, env.elsaPos[1], pxCurrent)
+            }
+
+        }
+    
+            env.elsaPos[1] += i
+        
+
+    // movementJump[1]= 10
+
+        console.log(pixelAtFeet)        
 
         if (movementChange) {
           env.elsaPos[0] += movementChange[0]// + movementJump[0];
