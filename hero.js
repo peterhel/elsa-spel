ddf88fcfb2f105050ee7e0ae80b7ef43bd4208f8
Peter Helenefors <peter@hertigcarl.se>
2022-02-01 22:03:19 +0100

    navigeringsmys +spegeltjej

--- hero.js
+++ hero.js
@@ -21,15 +21,37 @@ const groundMask = 0xffff0000;
 const opacityMask = 0x000000ff;
 
 class Hero {
+  images = {};
+
   constructor() {
-    this.drawing = new Image();
-    this.drawing.src = "res/elsa.png";
-    this.drawing.onload = () => {
+    let drawing = new Image();
+    drawing.src = "res/elsa.png";
+    let imgFlip = new Image();
+    imgFlip.src = "res/elsa_flip.png";
+
+
+    let load1 = new Promise((resolve) => {
+      drawing.onload = () => {
+        this.images[1] = drawing;
+
+        resolve();
+      };
+    });
+
+    let load2 = new Promise((resolve) => {
+      imgFlip.onload = () => {
+        this.images[-1] = imgFlip;
+
+        resolve();
+      };
+    });
+
+    Promise.all([load1, load2]).then(() => {
       if (this.onload) {
         this.onload = this.onload.bind(this);
         this.onload();
       }
-    };
+    });
   }
 
   onload = null;
@@ -37,26 +59,38 @@ class Hero {
     return this.drawing.height;
   }
 
+  lastPolarity = 1;
+
   render(ctx, env) {
     let movementChange = env.movementSpec;
+    let horizontalPolarity = movementChange[0] / Math.abs(movementChange[0]);
+    if(isNaN(horizontalPolarity)) {
+        horizontalPolarity = this.lastPolarity;
+    }
 
-    let pxHeroCenter = env.elsaPos[0] + this.drawing.width / 2;
-    let pxHeroBottom = env.elsaPos[1] + this.drawing.height;
+    this.lastPolarity = horizontalPolarity
+
+    let drawing = this.images[horizontalPolarity]
+
+    let pxHeroCenter = env.elsaPos[0] + drawing.width / 2;
+    let pxHeroBottom = env.elsaPos[1] + drawing.height;
 
     let i = 0;
 
-    if (movementChange[1] !== 0) { //If moving up or down
+    if (movementChange[1] !== 0) {
+      //If moving up or down
       let pm = movementChange[1] / Math.abs(movementChange[1]);
 
-      let feetZone = arrayToInt(ctx.getImageData(pxHeroCenter, pxHeroBottom + 1 * pm, 1, 1).data)
+      let feetZone = arrayToInt(
+        ctx.getImageData(pxHeroCenter, pxHeroBottom + 1 * pm, 1, 1).data
+      );
 
       while ((feetZone & ladderMask) >>> 0 > 0) {
         i = i + 1;
         feetZone = arrayToInt(
-            ctx.getImageData(pxHeroCenter, pxHeroBottom + (i + 1) * pm, 1, 1)
-              .data
-          )
-        
+          ctx.getImageData(pxHeroCenter, pxHeroBottom + (i + 1) * pm, 1, 1).data
+        );
+
         if (i >= env.stepLength) {
           break;
         }
@@ -70,17 +104,27 @@ class Hero {
     // );
     // let groundValue = (pxCurrent & groundMask) >>> 0;
 
-    let [r1,atFeet,,,r2,belowFeet] = ctx.getImageData(pxHeroCenter, pxHeroBottom-1, 1, 2).data
-    let freeFall = arrayToInt([r1,atFeet, r2,belowFeet])
+    let [r1, atFeet, , , r2, belowFeet] = ctx.getImageData(
+      pxHeroCenter,
+      pxHeroBottom - 1,
+      1,
+      2
+    ).data;
+    let freeFall = arrayToInt([r1, atFeet, r2, belowFeet]);
     // console.log(freeFall)
 
     //while (freeFall !== 65280 && freeFall !== 4278255360) {
-    while (freeFall === 0 || freeFall === 16711935 ||freeFall=== 16711680) {
-    //   console.log("falling");
+    while (freeFall === 0 || freeFall === 16711935 || freeFall === 16711680) {
+      //   console.log("falling");
       j++;
-      let [,atFeet,,,,belowFeet] = ctx.getImageData(pxHeroCenter, pxHeroBottom-1+j, 1, 2).data
-       freeFall = arrayToInt([atFeet, belowFeet])
-        if (j > env.stepLength) break;
+      let [, atFeet, , , , belowFeet] = ctx.getImageData(
+        pxHeroCenter,
+        pxHeroBottom - 1 + j,
+        1,
+        2
+      ).data;
+      freeFall = arrayToInt([atFeet, belowFeet]);
+      if (j > env.stepLength) break;
     }
 
     env.elsaPos[1] += j;
@@ -89,8 +133,8 @@ class Hero {
       env.elsaPos[0] += movementChange[0];
     }
 
-    if (env.elsaPos[0] + this.drawing.width > env.canvas.width) {
-      elsaPos[0] = env.canvas.width - this.drawing.width;
+    if (env.elsaPos[0] + drawing.width > env.canvas.width) {
+      elsaPos[0] = env.canvas.width - drawing.width;
     } else {
     }
 
@@ -98,6 +142,10 @@ class Hero {
       env.elsaPos[0] = 0;
     }
 
-    ctx.drawImage(this.drawing, env.elsaPos[0], env.elsaPos[1]);
+    ctx.drawImage(
+      drawing,
+      env.elsaPos[0],
+      env.elsaPos[1]
+    );
   }
 }
