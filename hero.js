2bef07b247e59a5eea46ab61ab3d28ab2c2bcd19
Peter Helenefors <peter@hertigcarl.se>
2022-02-01 17:28:14 +0100

    movement lite beeeehdre

--- hero.js
+++ hero.js
@@ -45,51 +45,42 @@ class Hero {
 
     let i = 0;
 
-    if (movementChange[1] !== 0) {
+    if (movementChange[1] !== 0) { //If moving up or down
       let pm = movementChange[1] / Math.abs(movementChange[1]);
-      console.log(movementChange, pm > 0 ? "down" : "up");
 
-      let feetZone = [
-        arrayToInt(
-          ctx.getImageData(pxHeroCenter, pxHeroBottom + 1 * pm, 1, 1).data
-        ),
-      ];
-      while (feetZone.some((x) => (x & ladderMask) >>> 0 > 0)) {
+      let feetZone = arrayToInt(ctx.getImageData(pxHeroCenter, pxHeroBottom + 1 * pm, 1, 1).data)
+
+      while ((feetZone & ladderMask) >>> 0 > 0) {
         i = i + 1;
-        console.log(
-          "At ladder. Eg accept up/downwards movement. i: ",
-          i,
-          env.stepLength
-        );
-        feetZone = [
-          arrayToInt(
+        feetZone = arrayToInt(
             ctx.getImageData(pxHeroCenter, pxHeroBottom + (i + 1) * pm, 1, 1)
               .data
-          ),
-        ];
+          )
+        
         if (i >= env.stepLength) {
-          console.log("reached stepLength");
           break;
         }
       }
-      console.log("before", env.elsaPos[1]);
       env.elsaPos[1] += i * pm;
-      console.log("asfter", env.elsaPos[1]);
     }
 
     let j = 0;
-    let pxCurrent = arrayToInt(
-      ctx.getImageData(pxHeroCenter, pxHeroBottom + j, 1, 1).data
-    );
-    let groundValue = (pxCurrent & groundMask) >>> 0;
-    while (groundValue === 0) {
-      console.log("falling");
+    // let pxCurrent = arrayToInt(
+    //   ctx.getImageData(pxHeroCenter, pxHeroBottom + j, 1, 1).data
+    // );
+    // let groundValue = (pxCurrent & groundMask) >>> 0;
+
+    let [r1,atFeet,,,r2,belowFeet] = ctx.getImageData(pxHeroCenter, pxHeroBottom-1, 1, 2).data
+    let freeFall = arrayToInt([r1,atFeet, r2,belowFeet])
+    // console.log(freeFall)
+
+    //while (freeFall !== 65280 && freeFall !== 4278255360) {
+    while (freeFall === 0 || freeFall === 16711935 ||freeFall=== 16711680) {
+    //   console.log("falling");
       j++;
-      pxCurrent = arrayToInt(
-        ctx.getImageData(pxHeroCenter, pxHeroBottom + j, 1, 1).data
-      );
-      groundValue = (pxCurrent & groundMask) >>> 0;
-      if (j > env.stepLength) break;
+      let [,atFeet,,,,belowFeet] = ctx.getImageData(pxHeroCenter, pxHeroBottom-1+j, 1, 2).data
+       freeFall = arrayToInt([atFeet, belowFeet])
+        if (j > env.stepLength) break;
     }
 
     env.elsaPos[1] += j;
