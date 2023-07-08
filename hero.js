53ab60c331239a7511821bb7bd1c0d1c5bc2bdb1
Peter <peter@hertigcarl.se>
2023-07-08 21:07:56 +0200

    reverted elsa pic

--- hero.js
+++ hero.js
@@ -25,10 +25,11 @@ class Hero {
 
   constructor() {
     let drawing = new Image();
-    drawing.src = "res/unicorn.png";
     let imgFlip = new Image();
-    imgFlip.src = "res/unicorn_flip.png";
-
+    drawing.src = "res/elsa.png";
+    imgFlip.src = "res/elsa_flip.png";
+    // drawing.src = "res/unicorn.png";
+    // imgFlip.src = "res/unicorn_flip.png";
 
     let load1 = new Promise((resolve) => {
       drawing.onload = () => {
